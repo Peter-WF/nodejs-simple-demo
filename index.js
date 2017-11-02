d5bccbeae2f33a22a7f957456000483604958a89
王斐 <wangfeia@zbj.com>
2017-11-02 18:50:05 +0800

    demo: add REST API demo

--- index.js
+++ index.js
@@ -16,10 +16,77 @@ MS.init({
 })
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.send('hello world')
 })
 
-app.listen(3000, function () {
+// get all project
+app.get('/api/projects/$', function(req, res) {
+  res.send({
+    success: true,
+    data: [{
+      id: '1',
+      name: 'project name',
+      description: 'my project'
+    }, {
+      id: '2',
+      name: 'project name2',
+      description: 'my project2'
+    }, {
+      id: '3',
+      name: 'project name3',
+      description: 'my project3'
+    }]
+  })
+})
+
+// create a project
+app.post('/api/projects/$', function(req, res) {
+  res.send({
+    success: true,
+    data: {
+      id: '4',
+      name: 'new project name',
+      description: 'new project'
+    }
+  })
+})
+
+// get project by projectId
+app.get('/api/projects/:projectId/$', function(req, res) {
+  res.send({
+    success: true,
+    data: {
+      id: '4',
+      name: 'new project name',
+      description: 'new project'
+    }
+  })
+})
+
+// update project by projectId
+app.patch('/api/projects/:projectId/$', function(req, res) {
+  res.send({
+    success: true,
+    data: {
+      id: '4',
+      name: 'new project name',
+      description: 'new project description'
+    }
+  })
+})
+
+// delete project by projectId
+app.delete('/api/projects/:projectId/$', function(req, res) {
+  res.send({
+    success: true
+  })
+})
+
+app.get('/', function(req, res) {
+  res.send('hello world')
+})
+
+app.listen(3000, function() {
   console.log('Example app listening on port 3000!')
 })
