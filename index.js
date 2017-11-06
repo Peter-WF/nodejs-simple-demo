e7f514b12ab1133888134c9a1a2753859c18f591
王斐 <wangfeia@zbj.com>
2017-11-06 18:49:13 +0800

    fix: init cors before MS.intercept

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ require('nodejs-mock-server')
 
 const utils = require('./utils')
 
-const config = require('./config.json')
-
 const app = express()
 
 MS.init({
@@ -20,22 +18,7 @@ MS.init({
 })
 
 // add intercept
-app.use('/api/*', MS.intercept(), function(req, res, next) {
-  // 如果 referer 不存在那么返回 404，暂时不添加该限制
-  // if (!req.headers.referer) {
-  //   res.sendStatus(403)
-  //   return
-  // }
-  const isAllowed = config.apiOriginList.split(',').some(item => {
-    return req.headers.referer.indexOf(item) === 0
-  })
-  if (!isAllowed) {
-    res.sendStatus(403)
-    return
-  }
-  utils.cors(req, res)
-  next()
-})
+app.use('/api/*', utils.authority, utils.cors, MS.intercept())
 
 // get all project
 app.get('/api/projects/$', function(req, res) {
--- utils.js
+++ utils.js
@@ -6,9 +6,10 @@
  */
 
 'use strict';
+const config = require('./config.json')
 
 module.exports = {
-  cors(req, res) {
+  cors(req, res, next) {
     if (req.headers['access-control-request-headers'] && req.headers['access-control-request-headers'] === 'content-type') {
       // 如果浏览器请求包括Access-Control-Request-Headers字段，则Access-Control-Allow-Headers字段是必需的。
       // 因为 patch 请求包含该字段并值为：content-type，所以服务端也设置该字段。
@@ -17,5 +18,20 @@ module.exports = {
     res.header('Access-Control-Allow-Credentials', true)
     res.header('Access-Control-Allow-Origin', req.headers.origin || '*')
     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE')
+    next()
+  },
+  authority(req, res, next) {
+    // 如果 referer 存在则进行 referer 校验
+    if (req.headers.referer) {
+      const isAllowed = config.apiOriginList.split(',').some(item => {
+        return req.headers.referer.indexOf(item) === 0
+      })
+      if (!isAllowed) {
+        res.sendStatus(403)
+        return
+      }
+    }
+
+    next()
   }
 }
