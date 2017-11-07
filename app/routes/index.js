634bdda11fbf465af6998c8202a284bffacc0284
王斐 <wangfeia@zbj.com>
2017-11-07 18:39:13 +0800

    fix: 解决项目目录接口调整后，项目根路径错误的问题

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,6 +6,7 @@
  */
 
 const express = require('express')
+const path = require('path')
 require('nodejs-mock-server')
 
 const utils = require('../utils')
@@ -14,7 +15,7 @@ const app = express()
 
 MS.init({
   app,
-  appDir: __dirname
+  appDir: path.join(__dirname,'../../')
 })
 
 // add intercept
